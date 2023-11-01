3f429bd3c8080d7a0ce6826cd6fe5b5a04ac8307
LilPolPol <paulhondola@gmail.com>
2023-11-01 15:11:39 +0200

    update rotate

---
+++ ex_bits/rotate_right.c
@@ -0,0 +1,29 @@
+#include <stdint.h>
+#include <stdio.h>
+
+void show_bits_32(uint32_t num)
+{
+    uint32_t mask = 1 << 31;
+
+    while(mask)
+    {
+        printf("%hhu ", (num & mask) != 0);
+        mask >>= 1;
+    }
+    printf("\n");
+}
+
+uint32_t rotate_right(uint32_t input)
+{
+    return (input << 1) | ((input & (1 << 31)) != 0);
+}
+
+int main(void)
+{
+    uint32_t num = 0;
+    scanf("%u", &num);
+    show_bits_32(num);
+    show_bits_32(rotate_right(num));
+
+    return 0;
+}
