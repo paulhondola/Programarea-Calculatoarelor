8af76d9f2ab029c58d3e0d265253f7309d343885
Paul Hondola <paulhondola@gmail.com>
2023-11-02 16:52:49 +0200

    update vectori

---
+++ ex_vectori/5.c
@@ -0,0 +1,5 @@
+// Se citesc 2 numere x și y cu x<y. Să se afișeze minimul elementelor din vectorul
+// {7, -5, 4, 3, -9, 2, -8}, care se află în același timp și în intervalul [x,y].
+
+#include <stdio.h>
+#include <stdint.h>
--- ex_vectori_pointeri/4.c
+++
@@ -1,3 +0,0 @@
-// Se citește un număr k.
-// Să se afișeze dacă în vectorul {7, -5, 4, 3, -9, 2, -8}
-// există cel puțin un număr mai mare decat k.
