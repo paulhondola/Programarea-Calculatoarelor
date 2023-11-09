5bf19cd5a0c8eda333f35e01f2fb2da15563b8ef
Paul Hondola <paulhondola@gmail.com>
2023-11-09 15:15:50 +0200

    Update lab7

--- lab/7/1.c
+++ lab/7/1.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
 
+void read_array(int *array, unsigned size)
+{
+    for(unsigned i = 0; i < size; i++)
+        scanf("%d", array + i);
+}
+
+
+void print_array(int *array, unsigned size)
+{
+    for(unsigned i = 0; i < size; i++)
+        printf("%d ", *(array + i));
+    printf("\n");
+}
+
+
 int main(void)
 {
+    unsigned size = 0;
+    scanf("%u", &size);
+
+    int array[size];
+    read_array(array, size);
+
+    int *ptr = array + 2;
+    *array = *ptr;
+    print_array(array, size);
+
     return 0;
 }
