bb2755cdd01956680ce8c1ba30a3d12307859db2
Paul Hondola <paulhondola@gmail.com>
2023-11-28 12:35:44 +0200

    Update curs 10

--- curs/10/fisiere.c
+++ curs/10/fisiere.c
@@ -28,14 +28,18 @@ int main(void)
     cursorul va citi de pe pozitia in care a ramas ultima data
 
     cursorul semnaleaza EOF (end of file) cand ajunge la finalul fisierului
+
+
     */
+
+
     FILE *f = NULL;
 
     // nu ii pot accesa membrii la structura
 
     if ((f = fopen("input.txt", "r")) == NULL)
     {
-        perror("eroare deschidere fisier");
+        perror("eroare deschidere fisier\n");
         exit(-1);
     }
     else
@@ -44,14 +48,73 @@ int main(void)
     }
 
 
+    // fgetc - citeste un caracter din fisier - returneaza EOF cand ajunge la finalul fisierului
+
+    /*
+    char c = 0;
+
+    while ((c = fgetc(f)) != EOF)
+    {
+        printf("%c", c);
+    }
+    */
+
+    // fgets - citeste o linie din fisier - returneaza NULL cand ajunge la finalul fisierului
+
+    // fgets - citeste si \n
+    char *s = NULL;
+    
+    s = malloc(100 * sizeof(char));
+
+    while ((s = fgets(s, 100, f)) != NULL)
+    {
+        printf("%s", s);
+    }
+
+
+    /*
+    
+    fputc - scrie un caracter in fisier
+
+    ungetc - pune un caracter inapoi in fisier
+
+    fputs - scrie un string in fisier
+
+
+
+    feof - verifica daca cursorul a ajuns la finalul fisierului - returneaza 0 sau 1
+
+
+    fprintf - scrie in fisier la fel ca printf
+
+
+
+    */
+
+    fscanf(f, "%s", s);
+
+    fprintf(f, "ceva\n");
+
+    sprintf(s, "ceva\n"); // scrie in s la fel ca printf
+
+
+    sscanf(s, "%s", s); // citeste din s la fel ca scanf
+
+
+
+
+
+
+
+
     if(fclose(f) == EOF)
     {
-        perror("eroare inchidere fisier");
+        perror("\n\neroare inchidere fisier");
         exit(-1);
     }
     else
     {
-        printf("fisier inchis cu succes\n");
+        printf("\nfisier inchis cu succes\n");
     }
 
     return 0;
---
+++ curs/10/histograma.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SIZE 2000
+#define ASCII_COUNT 255
+
+
+int histograma(char *line, int *tab)
+{
+    int count = 0;
+
+    for(int i = 0; i < strlen(line); i++)
+    {
+        tab[(int)line[i]]++;
+        count++;
+    }
+
+    return count;
+}
+
+
+void print_histograma(int *tab, int count)
+{
+    for(int i = 32; i < 127; i++)
+    {
+        printf("%c -> %d -> %.3f\n", i, tab[i], 100 * (float)tab[i] / count);
+    }
+}
+
+int main(void)
+{
+    FILE *file = NULL;
+
+    if((file = fopen("input.txt", "r")) == NULL)
+    {
+        perror("eroare deschidere fisier\n");
+        exit(-1);
+    }
+
+
+
+
+    int COUNT = 0;
+
+    int tab[ASCII_COUNT];
+    memset(tab, 0, sizeof(tab));
+
+    char line[SIZE];
+
+    while((fgets(line, SIZE, file)) != NULL)
+    {
+        COUNT += histograma(line, tab);
+
+        printf("%s", line);
+    }
+
+    printf("Total caractere: %d\n", COUNT);
+
+    print_histograma(tab, COUNT);
+
+    if(fclose(file) == EOF)
+    {
+        perror("eroare inchidere fisier\n");
+        exit(-1);
+    }
+
+    return 0;
+}
