41ce5244d87b8f6db5cb326f130672951fcde1b6
Paul Hondola <paulhondola@gmail.com>
2024-01-28 16:00:35 +0200

    examen upper

--- exercitii_examen/upper/main.c
+++ exercitii_examen/upper/main.c
@@ -5,3 +5,72 @@ Să se implementeze o funcție void transform(const char *srcfile, const char *d
 Sa se încerce problema de mai sus și pentru transformarea tuturor literelor mici în litere mari, indiferent de poziția lor în șir (nu mai este necesar să transformăm doar prima literă)
 */
 
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+#define CHUNK 1024
+
+FILE *open_file(const char *filepath, const char *mode)
+{
+    FILE *file = fopen(filepath, mode);
+    if (file == NULL)
+    {
+        perror(filepath);
+        exit(EXIT_FAILURE);
+    }
+    return file;
+}
+
+
+void close_file(FILE *file)
+{
+    if (fclose(file) == EOF)
+    {
+        perror("fclose");
+        exit(EXIT_FAILURE);
+    }
+}
+
+
+void transform(const char *srcfile, const char *destfile)
+{
+    FILE *input = open_file(srcfile, "r");
+    FILE *output = open_file(destfile, "w");
+
+    char line[CHUNK];
+    char *token;
+    while(1)
+    {
+        if(fgets(line, CHUNK, input) == NULL)
+            break;
+
+        if(line[strlen(line) - 1] == '\n')
+            line[strlen(line) - 1] = '\0';
+
+        token = strtok(line, " ");
+
+        while(token != NULL)
+        {
+            if(isalpha(token[0]))
+                token[0] = toupper(token[0]);
+
+            fprintf(output, "%s ", token);
+            token = strtok(NULL, " ");
+        }
+
+        fprintf(output, "\n");
+    }
+
+    close_file(input);
+    close_file(output);
+}
+
+
+int main(void)
+{
+    transform("input.txt", "output.txt");
+
+    return 0;
+}
